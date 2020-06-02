fe74529a18831ca42a2ec51c4b96d0feb4d0b1a9
Gabriel Prando <gprando55@gmail.com>
2020-06-02 14:05:05 -0300

    add controllers and imgs

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
+import ItemsController from './controllers/ItemsController';
+import PointController from './controllers/PointController';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
+routes.get('/items', ItemsController.index);
 
-  return res.json({ message: "hellow world" });
-});
+routes.post('/points', PointController.store);
+routes.get('/points/:id', PointController.show);
+routes.get('/points', PointController.index);
 
 export default routes;
